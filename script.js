db7d7de3d52784299b65923bedb0e7439df9886a
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-14 13:04:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -199,6 +199,22 @@ if ("serviceWorker" in navigator) {
 
     navigator.serviceWorker.register(
         "./service-worker.js"
-    );
+    )
+    .then(function (registration) {
+
+        console.log(
+            "MARA SW BERHASIL:",
+            registration.scope
+        );
+
+    })
+    .catch(function (error) {
+
+        console.error(
+            "MARA SW GAGAL:",
+            error
+        );
+
+    });
 
 }
