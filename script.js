6596b62e9a76946e9974057eb4d3900e5ed5dbc0
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-14 12:52:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -195,10 +195,10 @@ controlCenterOverlay.addEventListener(
     },
     { passive: true }
 );
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(reg => console.log('Service Worker registered!', reg))
-      .catch(err => console.error('Service Worker registration failed:', err));
-  });
+if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker.register(
+        "./service-worker.js"
+    );
+
 }
