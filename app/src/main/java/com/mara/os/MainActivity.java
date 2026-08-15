871d395584a821b05147c817a74159034288b442
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-16 00:14:54 +0700

    Update MainActivity.java

--- app/src/main/java/com/mara/os/MainActivity.java
+++ app/src/main/java/com/mara/os/MainActivity.java
@@ -2,6 +2,7 @@ package com.mara.os;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,6 +14,16 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Fullscreen immersive
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
 
         webView = new WebView(this);
 
@@ -27,14 +38,5 @@ public class MainActivity extends Activity {
         webView.loadUrl("file:///android_asset/index.html");
 
         setContentView(webView);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
