7a610d0211e156a807cadd8fe30603014105a926
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-16 00:54:19 +0700

    Update MainActivity.java

--- app/src/main/java/com/mara/os/MainActivity.java
+++ app/src/main/java/com/mara/os/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -19,13 +20,13 @@ public class MainActivity extends Activity {
 
         Window window = getWindow();
 
-        // Membuat aplikasi menggambar sampai seluruh layar
+        // Jangan sisakan area untuk status bar dan navigation bar
         window.setFlags(
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
 
-        // Fullscreen immersive
+        // Sembunyikan system UI
         window.getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -45,8 +46,18 @@ public class MainActivity extends Activity {
 
         webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl("file:///android_asset/index.html");
+        // Pastikan WebView memenuhi seluruh window
+        webView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
 
         setContentView(webView);
+
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
