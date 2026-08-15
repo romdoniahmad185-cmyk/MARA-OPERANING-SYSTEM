622d02c9a10c8ecd99ad1dc3066600844be2f6f0
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-16 00:43:36 +0700

    Update MainActivity.java

--- app/src/main/java/com/mara/os/MainActivity.java
+++ app/src/main/java/com/mara/os/MainActivity.java
@@ -3,6 +3,8 @@ package com.mara.os;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -15,8 +17,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Window window = getWindow();
+
+        // Membuat aplikasi menggambar sampai seluruh layar
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
         // Fullscreen immersive
-        getWindow().getDecorView().setSystemUiVisibility(
+        window.getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
