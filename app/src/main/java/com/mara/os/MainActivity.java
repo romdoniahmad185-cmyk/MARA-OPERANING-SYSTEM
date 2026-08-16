bccca59f57e66d2ba3d1bfbb9d90dc902babae66
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-16 15:11:35 +0700

    Update MainActivity.java

--- app/src/main/java/com/mara/os/MainActivity.java
+++ app/src/main/java/com/mara/os/MainActivity.java
@@ -4,85 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-
-    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Window window = getWindow();
+        View screen = new View(this);
+        screen.setBackgroundColor(Color.WHITE);
 
-        // Status bar transparan agar menyatu dengan MARA OS
-        window.setStatusBarColor(Color.TRANSPARENT);
-
-        // Navigation bar transparan
-        window.setNavigationBarColor(Color.TRANSPARENT);
-
-        // WebView menggambar sampai area system bar
-        window.setDecorFitsSystemWindows(false);
-
-        // Konfigurasi warna/icon system bar
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-
-            WindowInsetsController controller =
-                    window.getInsetsController();
-
-            if (controller != null) {
-                controller.setSystemBarsAppearance(
-                        0,
-                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-                                | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                );
-            }
-        }
-
-        // Membuat WebView
-        webView = new WebView(this);
-
-        WebSettings settings = webView.getSettings();
-
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-
-        // WebView menggunakan client internal
-        webView.setWebViewClient(new WebViewClient());
-
-        // Background transparan supaya UI MARA yang mengatur tampilan
-        webView.setBackgroundColor(Color.TRANSPARENT);
-
-        // Izinkan WebView menerima area layar penuh
-        webView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        );
-
-        // Memuat MARA OS dari assets
-        webView.loadUrl(
-                "file:///android_asset/index.html"
-        );
-
-        setContentView(webView);
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        setContentView(screen);
     }
 }
