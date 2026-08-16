9e19c88067017bc071f7a6f019f2e59f9f63eda9
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-16 15:21:00 +0700

    Update MainActivity.java

--- app/src/main/java/com/mara/os/MainActivity.java
+++ app/src/main/java/com/mara/os/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +14,41 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Window window = getWindow();
+
+        // Membuat layar benar-benar fullscreen
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+
+            WindowInsetsController controller =
+                    window.getInsetsController();
+
+            if (controller != null) {
+
+                controller.hide(
+                        WindowInsets.Type.statusBars()
+                                | WindowInsets.Type.navigationBars()
+                );
+
+                controller.setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+            }
+
+            window.setDecorFitsSystemWindows(false);
+
+        } else {
+
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
+
+        // Layar sementara MARA
         View screen = new View(this);
         screen.setBackgroundColor(Color.WHITE);
 
