9f6a020ca0ea776f932d0e3364b2de60848bdfbd
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-16 15:30:16 +0700

    Update MainActivity.java

--- app/src/main/java/com/mara/os/MainActivity.java
+++ app/src/main/java/com/mara/os/MainActivity.java
@@ -4,54 +4,61 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Window window = getWindow();
+        webView = new WebView(this);
 
-        // Membuat layar benar-benar fullscreen
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+        WebSettings settings = webView.getSettings();
 
-            WindowInsetsController controller =
-                    window.getInsetsController();
+        // MARA OS menggunakan HTML/CSS/JavaScript lokal
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
 
-            if (controller != null) {
+        // Izinkan file lokal dari assets
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
 
-                controller.hide(
-                        WindowInsets.Type.statusBars()
-                                | WindowInsets.Type.navigationBars()
-                );
+        // Tetap berada di dalam aplikasi MARA
+        webView.setWebViewClient(new WebViewClient());
 
-                controller.setSystemBarsBehavior(
-                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                );
-            }
+        // Background sementara jika HTML belum selesai dimuat
+        webView.setBackgroundColor(Color.BLACK);
 
-            window.setDecorFitsSystemWindows(false);
+        // Muat UI MARA dari APK
+        webView.loadUrl("file:///android_asset/index.html");
 
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
         } else {
+            super.onBackPressed();
+        }
+    }
 
-            window.getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            );
+    @Override
+    protected void onDestroy() {
+
+        if (webView != null) {
+            webView.loadUrl("about:blank");
+            webView.stopLoading();
+            webView.destroy();
         }
 
-        // Layar sementara MARA
-        View screen = new View(this);
-        screen.setBackgroundColor(Color.WHITE);
-
-        setContentView(screen);
+        super.onDestroy();
     }
 }
