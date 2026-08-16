2ca86ee689c9b18bf67d9d052e2b6a8446a738f6
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-17 02:18:24 +0700

    Update MainActivity.java

--- app/src/main/java/com/mara/os/MainActivity.java
+++ app/src/main/java/com/mara/os/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.graphics.Color;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -28,28 +26,12 @@ public class MainActivity extends Activity {
 
         window.setStatusBarColor(Color.TRANSPARENT);
 
-        if (android.os.Build.VERSION.SDK_INT >= 30) {
-
-            window.setDecorFitsSystemWindows(false);
-
-            WindowInsetsController controller =
-                    window.getInsetsController();
-
-            if (controller != null) {
-                controller.hide(
-                        WindowInsets.Type.statusBars()
-                );
-            }
-
-        } else {
-
-            window.getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            );
-        }
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
 
         // ==========================================
         // MARA WEB RUNTIME
